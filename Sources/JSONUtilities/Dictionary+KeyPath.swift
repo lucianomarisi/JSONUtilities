3f998467f31745cb48c2cab3219b1eb88393ebdc
Luciano Marisi <luciano@techbrewers.com>
2016-09-04 21:44:51 +0100

    Keypath extension on dictionary now uses Any instead of AnyObject

--- Sources/JSONUtilities/Dictionary+KeyPath.swift
+++ Sources/JSONUtilities/Dictionary+KeyPath.swift
@@ -15,18 +15,18 @@ extension Dictionary {
   /// - parameter keyPath: A string of keys separated by dots
   ///
   /// - returns: Optionally returns a generic value for this keyPath or nil
-  subscript(keyPath keyPath: StringProtocol) -> AnyObject? {
+  subscript(keyPath keyPath: StringProtocol) -> Any? {
     var keys = keyPath.components(separatedBy: ".")
     guard let first = keys.first as? Key,
           let value = self[first]
       else { return nil }
     keys.removeFirst()
     
-    if !keys.isEmpty, let subDictionary = value as? [NSObject : AnyObject] {
+    if !keys.isEmpty, let subDictionary = value as? [NSObject : Any] {
       let rejoined = keys.joined(separator: ".")
       return subDictionary[keyPath: rejoined]
     }
-    return value as AnyObject
+    return value
   }
   
 }
