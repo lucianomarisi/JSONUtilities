4d84ad982ac35ff944111e7952bc08ec7f8c1ecc
Sam Dods <sam@theappbusiness.com>
2016-09-14 10:27:29 +0100

    documented new method

--- Sources/JSONUtilities/URL+JSONPrimitiveConvertible.swift
+++ Sources/JSONUtilities/URL+JSONPrimitiveConvertible.swift
@@ -12,6 +12,11 @@ extension URL: JSONPrimitiveConvertible {
   
   public typealias JSONType = String
   
+  /// Creates a URL from a string, in order to conform to JSONPrimitiveConvertible
+  ///
+  /// - parameter jsonValue: The string representation of a valid URL
+  ///
+  /// - returns: Returns a URL if the input string was successfully converted to a URL
   public static func from(jsonValue: String) -> URL? {
     return URL(string: jsonValue)
   }
