b68c8f47c6bdde6f8aeeaa6b082c26c481cd0816
Yonas Kolb <yonaskolb@gmail.com>
2017-04-26 22:29:05 +0800

    add DecodingError docs

--- Sources/JSONUtilities/DecodingError.swift
+++ Sources/JSONUtilities/DecodingError.swift
@@ -9,17 +9,26 @@
 import Foundation
 
 /**
- Decoding error for mandatory keys
-
- - Mandatory: The key that failed the decoding
+ Error that occurs when decoding
  */
 public struct DecodingError: Error, CustomStringConvertible, CustomDebugStringConvertible {
 
+  /// The reason the error occurred
   public var reason: Reason
+
+  /// dictionary in which the error occured
   public let dictionary: [AnyHashable: Any]
+
+  /// array in which the error occurred
   public let array: JSONArray?
+
+  /// the keypath at which the error occurred
   public let keyPath: String
+
+  /// the expected type that was being decoded while the error happened
   public let expectedType: Any
+
+  /// the value that caused the error
   public let value: Any
 
   init(dictionary: [AnyHashable: Any], keyPath: StringProtocol, expectedType: Any.Type, value: Any, array: JSONArray? = nil, reason: Reason) {
@@ -52,11 +61,19 @@ public struct DecodingError: Error, CustomStringConvertible, CustomDebugStringCo
     return "\(description):\n\(array?.description ?? dictionary.description)"
   }
 
+  /// The reason the error occurred
   public enum Reason: String, CustomStringConvertible {
 
+    /// Key was not found in a JSONDictionary
     case keyNotFound = "Key not found"
+
+    /// A value was found that can't initialise a RawRepresentable. In the case of an enum, the rawValue did not match any of the case's rawValue
     case incorrectRawRepresentableRawValue = "Incorrect RawRepresentable RawValue"
+
+    /// The value has the incorrect type
     case incorrectType = "Incorrect type"
+
+    /// A JSONPrimitiveConvertable failed to convert
     case conversionFailure = "Conversion failure"
 
     public var description: String {
